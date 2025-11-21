cf189c366c8e08594de1ee63ca152c1dc7f31898
julynguyen247 <julyneverdie@gmail.com>
2025-11-21 10:14:29 +0700

    fix bugs

--- be/apps/attempts/src/main.ts
+++ be/apps/attempts/src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
     {
       transport: Transport.RMQ,
       options: {
-        urls: [process.env.RABBITMQ_URL ?? ''],
+        urls: [
+          process.env.RABBITMQ_URL ?? 'amqp://admin:admin123@localhost:5672',
+        ],
         queue: process.env.ATTEMPTS_QUEUE ?? 'attempts_queue',
         queueOptions: {
           durable: false,
